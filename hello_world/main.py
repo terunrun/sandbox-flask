3f913fb45242787b55795e44e8f25d382ed33351
terunrun <terunrun@gmail.com>
2023-04-20 15:04:12 +0900

    add dockerfile for hello world

--- hello_world/main.py
+++ hello_world/main.py
@@ -1,11 +1,13 @@
-from flask import Flask, request
+import os
+
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello world!!</p>"
-
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
